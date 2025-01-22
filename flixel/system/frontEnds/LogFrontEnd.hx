c0787fcd4c4128cca5c50f0c77380841f87cbb05
Altertoriel <op58274@gmail.com>
2025-01-23 01:10:08 +0900

    logs update

--- flixel/system/frontEnds/LogFrontEnd.hx
+++ flixel/system/frontEnds/LogFrontEnd.hx
@@ -18,24 +18,24 @@ class LogFrontEnd
 
 	var _standardTraceFunction:(Dynamic, ?PosInfos)->Void;
 
-	public inline function add(data:Dynamic):Void
+	public inline function add(data:Dynamic, ?pos:PosInfos):Void
 	{
-		advanced(data, LogStyle.NORMAL);
+		advanced(data, LogStyle.NORMAL, false, pos);
 	}
 
-	public inline function warn(data:Dynamic):Void
+	public inline function warn(data:Dynamic, ?pos:PosInfos):Void
 	{
-		advanced(data, LogStyle.WARNING, true);
+		advanced(data, LogStyle.WARNING, true, pos);
 	}
 
-	public inline function error(data:Dynamic):Void
+	public inline function error(data:Dynamic, ?pos:PosInfos):Void
 	{
-		advanced(data, LogStyle.ERROR, true);
+		advanced(data, LogStyle.ERROR, true, pos);
 	}
 
-	public inline function notice(data:Dynamic):Void
+	public inline function notice(data:Dynamic, ?pos:PosInfos):Void
 	{
-		advanced(data, LogStyle.NOTICE);
+		advanced(data, LogStyle.NOTICE, false, pos);
 	}
 
 	/**
@@ -46,7 +46,7 @@ class LogFrontEnd
 	 * @param   fireOnce  Whether you only want to log the Data in case it hasn't been added already
 	 */
 	@:haxe.warning("-WDeprecated")
-	public function advanced(data:Any, ?style:LogStyle, fireOnce = false):Void
+	public function advanced(data:Any, ?style:LogStyle, fireOnce = false, ?pos:PosInfos):Void
 	{
 		if (style == null)
 			style = LogStyle.NORMAL;
@@ -78,7 +78,7 @@ class LogFrontEnd
 		}
 		#end
 		
-		style.onLog.dispatch(data);
+		style.onLog.dispatch(data, pos);
 		
 		if (style.throwException)
 			throw style.toLogString(arrayData);
