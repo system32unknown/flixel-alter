d2ad3e14d9ad49dee3934eb15cce1b5277db45dc
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-01-11 19:23:40 +0900

    f

--- flixel/util/FlxPool.hx
+++ flixel/util/FlxPool.hx
@@ -5,6 +5,34 @@ import flixel.util.FlxDestroyUtil;
 import flixel.util.typeLimit.OneOfTwo;
 
 /**
+ * Helper type that allows the `FlxPool` constructor to take the new function param and the old,
+ * deprecated `Class<T>` param. This will be removed, soon anf FlxPool will only take a function.
+ */
+abstract PoolFactory<T:IFlxDestroyable>(() -> T)
+{
+	@:from
+	#if FLX_NO_UNIT_TEST
+	@:deprecated("use `MyType.new` or `()->new MyType()` instead of `MyType`)")
+	#end
+	public static inline function fromClass<T:IFlxDestroyable>(classRef:Class<T>):PoolFactory<T>
+	{
+		return fromFunction(() -> Type.createInstance(classRef, []));
+	}
+	
+	@:from
+	public static inline function fromFunction<T:IFlxDestroyable>(func:() -> T):PoolFactory<T>
+	{
+		return cast func;
+	}
+	
+	@:allow(flixel.util.FlxPool)
+	inline function getFunction():() -> T
+	{
+		return this;
+	}
+}
+
+/**
  * A generic container that facilitates pooling and recycling of objects.
  * WARNING: Pooled objects must have parameter-less constructors: function new()
  */
