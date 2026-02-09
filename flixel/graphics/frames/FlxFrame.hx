fdc124c3f42ee755987305d94437c04566f90992
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-02-09 22:02:26 +0900

    dsdffgdg

--- flixel/graphics/frames/FlxFrame.hx
+++ flixel/graphics/frames/FlxFrame.hx
@@ -813,7 +813,7 @@ class FlxFrame implements IFlxDestroyable
 		if (uv == null)
 			uv = FlxRect.get();
 
-		uv.setFromFrameRect(frame, parent);
+		uv.set(value.x / parent.width, value.y / parent.height, value.right / parent.width, value.bottom / parent.height);
 	}
 }
 
