009f31ca735dc12efc6a0f25e2f0fb18c8b93eec
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-02-09 22:06:05 +0900

    Revert "g"
    This reverts commit 07fc50a2c28b31f7871fb11fd607f2d9460acce3.

--- flixel/graphics/frames/FlxFrame.hx
+++ flixel/graphics/frames/FlxFrame.hx
@@ -837,6 +837,68 @@ enum abstract FlxFrameAngle(Int) from Int to Int
 }
 
 /**
+ * FlxRect, but instead of `x`, `y`, `width` and `height`, it takes a `left`, `right`, `top` and
+ * `bottom`. This is for optimization reasons, to reduce arithmetic when drawing vertices
+ */
+@:forward(put)
+abstract FlxUVRect(FlxRect) from FlxRect to flixel.util.FlxPool.IFlxPooled
+{
+	public var left(get, set):Float;
+	inline function get_left():Float { return this.x; }
+	inline function set_left(value):Float { return this.x = value; }
+	
+	/** Top */
+	public var right(get, set):Float;
+	inline function get_right():Float { return this.width; }
+	inline function set_right(value):Float { return this.width = value; }
+	
+	/** Right */
+	public var top(get, set):Float;
+	inline function get_top():Float { return this.y; }
+	inline function set_top(value):Float { return this.y = value; }
+	
+	/** Bottom */
+	public var bottom(get, set):Float;
+	inline function get_bottom():Float { return this.height; }
+	inline function set_bottom(value):Float { return this.height = value; }
+	
+	public inline function set(l, t, r, b)
+	{
+		this.set(l, t, r, b);
+	}
+	
+	public inline function setFromFrameRect(frame:FlxRect, parent:FlxGraphic)
+	{
+		this.set(frame.x / parent.width, frame.y / parent.height, frame.right / parent.width, frame.bottom / parent.height);
+	}
+	
+	public inline function copyTo(uv:FlxUVRect)
+	{
+		uv.set(left, top, right, bottom);
+	}
+	
+	public inline function copyFrom(uv:FlxUVRect)
+	{
+		set(uv.left, uv.top, uv.right, uv.bottom);
+	}
+	
+	public inline function toString()
+	{
+		return return FlxStringUtil.getDebugString([
+			LabelValuePair.weak("l", left),
+			LabelValuePair.weak("t", top),
+			LabelValuePair.weak("r", right),
+			LabelValuePair.weak("b", bottom)
+		]);
+	}
+	
+	public static function get(l = 0.0, t = 0.0, r = 0.0, b = 0.0)
+	{
+		return FlxRect.get(l, t, r, b);
+	}
+}
+
+/**
  * Used internally instead of a FlxMatrix, for some unknown reason.
  * Perhaps improves performance, tbh, I'm skeptical
  */
