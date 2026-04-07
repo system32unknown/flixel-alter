e4dee2d17028232dc82d5a9dd7f930d10005badc
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-04-07 16:04:44 +0900

    f

--- flixel/system/frontEnds/SoundFrontEnd.hx
+++ flixel/system/frontEnds/SoundFrontEnd.hx
@@ -406,11 +406,6 @@ class SoundFrontEnd
 	 */
 	public function pause():Void
 	{
-		if (music != null && music.exists && music.active)
-		{
-			music.pause();
-		}
-
 		for (sound in list.members)
 		{
 			if (sound != null && sound.exists && sound.active)
@@ -425,11 +420,6 @@ class SoundFrontEnd
 	 */
 	public function resume():Void
 	{
-		if (music != null && music.exists)
-		{
-			music.resume();
-		}
-
 		for (sound in list.members)
 		{
 			if (sound != null && sound.exists)
@@ -446,12 +436,6 @@ class SoundFrontEnd
 	 */
 	public function destroy(forceDestroy = false):Void
 	{
-		if (music != null && (forceDestroy || !music.persist))
-		{
-			music.destroy();
-			music = null;
-		}
-
 		for (sound in list.members)
 		{
 			if (sound != null && (forceDestroy || !sound.persist))
@@ -539,9 +523,6 @@ class SoundFrontEnd
 	@:allow(flixel.FlxGame)
 	function update(elapsed:Float):Void
 	{
-		if (music != null && music.active)
-			music.update(elapsed);
-
 		if (list != null && list.active)
 			list.update(elapsed);
 
@@ -561,11 +542,6 @@ class SoundFrontEnd
 	@:allow(flixel.FlxGame)
 	function onFocusLost():Void
 	{
-		if (music != null)
-		{
-			music.onFocusLost();
-		}
-
 		for (sound in list.members)
 		{
 			if (sound != null)
@@ -578,11 +554,6 @@ class SoundFrontEnd
 	@:allow(flixel.FlxGame)
 	function onFocus():Void
 	{
-		if (music != null)
-		{
-			music.onFocus();
-		}
-
 		for (sound in list.members)
 		{
 			if (sound != null)
